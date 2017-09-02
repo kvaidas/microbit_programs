74be6dd99e2af123d7c7addd4d5fc2da3061f376
Vaidas Kascėnas <vaidas@kascenas.lt>
2017-09-02 23:48:43 +0300

    Update knight_rider.js

--- flashers/knight_rider.js
+++ flashers/knight_rider.js
@@ -1,30 +1,19 @@
+let pixels: game.LedSprite[] = []
 let delay = 0
-let y = 0
-let x = 0
 basic.forever(() => {
-    for (let x = 0; x <= 4; x++) {
-        for (let y = 0; y <= 4; y++) {
-            led.toggle(x, y)
-        }
-        basic.pause(delay)
-        for (let y = 0; y <= 4; y++) {
-            led.toggle(x, y)
-        }
+    for (let current_pixel of pixels) {
+        current_pixel.ifOnEdgeBounce()
+        current_pixel.move(1)
     }
-    for (let x = 0; x <= 4; x++) {
-        for (let y = 0; y <= 4; y++) {
-            led.toggle(4 - x, y)
-        }
-        basic.pause(delay)
-        for (let y = 0; y <= 4; y++) {
-            led.toggle(4 - x, y)
-        }
-    }
+    basic.pause(delay)
 })
 input.onButtonPressed(Button.A, () => {
-    delay = delay / 2
+    if (delay != 1) {
+        delay = delay / 2
+    }
 })
 input.onButtonPressed(Button.B, () => {
     delay = delay * 2
 })
 delay = 50
+pixels = [game.createSprite(0, 0), game.createSprite(0, 1), game.createSprite(0, 2), game.createSprite(0, 3), game.createSprite(0, 4)]
