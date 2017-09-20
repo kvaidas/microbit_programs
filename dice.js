1effdb4b892a70b5e9e9b0ffdd6f9eb4f360291b
Vaidas Kascėnas <vaidas@kascenas.lt>
2017-09-20 09:37:06 +0300

    Create dice.js

---
+++ dice.js
@@ -0,0 +1,53 @@
+let thrown = 0
+input.onGesture(Gesture.Shake, () => {
+    thrown = Math.random(6) + 1
+    if (thrown == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (thrown == 2) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (thrown == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (thrown == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (thrown == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (thrown == 6) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+})
